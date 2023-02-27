90c7e7c6543b62c40bece8da3d60be2c44a95152
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-02-27 16:01:48 -0500

    nice store and type update

---
+++ src/store.ts
@@ -0,0 +1,118 @@
+import { browser } from '$app/environment';
+import { writable } from 'svelte/store';
+import type { Point, PointUpdate, Recipe, RecipeUpdate } from './types/recipe';
+
+
+export const activeRecipe = writable('');
+
+const defaultValueRecipes: Record<string, Recipe> = {
+    kom: {
+        name: 'Kombucha',
+        id: 'kom',
+        points: [{
+            id: 'a', x: 2, y: 8, title: 'Apple&Cranberries',
+            details: 'Good one but not enough fermentaion'
+        },
+        {
+            id: 'b', x: 6, y: 8, title: 'Orange',
+            details: 'need a few more days, made with Ceylan tea'
+        }]
+    },
+    bread: {
+        name: 'Bread',
+        id: 'bread',
+        points: [{
+            id: 'c', x: 4, y: 8, title: '10% Buckwheat 10% Complete',
+            details: 'Not a lot of rising, 65% hydratation'
+        },
+        {
+            id: 'd', x: 6, y: 7, title: '20% Complete Wheat',
+            details: 'Good one but not enough fermentation'
+        }]
+    }
+};
+
+
+export const recipes = (() => {
+    const { subscribe, set, update } = writable<Record<string, Recipe>>(defaultValueRecipes);
+
+    function defaultUpdate(props: RecipeUpdate) {
+        return (recipe: Recipe) => {
+            return { ...recipe, ...props };
+        };
+    }
+
+
+    return {
+        subscribe,
+        set,
+        add: (recipe: Recipe) => {
+            update((recipes) => {
+                recipes[recipe.id] = recipe;
+                return recipes;
+            });
+        },
+        remove: (recipeId: string) => {
+            update((recipes) => {
+                delete recipes[recipeId]
+                return recipes
+            });
+        },
+        updateName: (recipeId: string, newName: string) => {
+            update((recipes) => {
+                recipes[recipeId] = { ...recipes[recipeId], name: newName }
+                return recipes
+            });
+        },
+        update: (props: RecipeUpdate, fn = defaultUpdate(props)) => {
+            update((recipes) => {
+                recipes[props.id] = fn(recipes[props.id])
+                return recipes
+            });
+        },
+    };
+})();
+
+
+
+
+export const points = {
+    add: (point: Point, recipeId: string) => {
+        recipes.update({ id: recipeId }, addPointToRecipe(point));
+    },
+    remove: (pointId: string, recipeId: string) => {
+        recipes.update({ id: recipeId }, removePointFromRecipe(pointId));
+    },
+    update: (pointUpdate: PointUpdate, recipeId: string) => {
+        recipes.update({ id: recipeId }, updatePointFromRecipe(pointUpdate));
+    },
+};
+
+
+function addPointToRecipe(point: Point) {
+    return (recipe: Recipe) => {
+        recipe.points.push(point);
+        return recipe;
+    };
+}
+
+
+function removePointFromRecipe(pointId: string) {
+    return (recipe: Recipe) => {
+        recipe.points.filter(({ id }) => id !== pointId)
+        return recipe;
+    };
+}
+
+
+function updatePointFromRecipe(pointUpdate: PointUpdate) {
+    return (recipe: Recipe) => {
+        recipe.points.map((point) => point.id == pointUpdate.id ? { ...point, ...pointUpdate } : point)
+        return recipe;
+    };
+}
+
+
+
+// const localStorageRecipes = browser && localStorage.recipes && JSON.parse(localStorage.recipes);
+
--- src/types/recipe.ts
+++ src/types/recipe.ts
@@ -1,3 +1,7 @@
+import type { Writable } from "svelte/store";
+
+type AtLeast<T, K extends keyof T> = Partial<T> & Pick<T, K>
+
 export type Point = {
     x: number;
     y: number;
@@ -7,17 +11,16 @@ export type Point = {
     date?: Date;
 };
 
-export type NewPoint = {
-    x?: number;
-    y?: number;
-    id?: string;
-    title?: string;
-    details?: string;
-    date?: Date;
-};
+export type NewPoint = Partial<Point>
 
-export  type Recipe = {
+export type PointUpdate = AtLeast<Point, 'id'>
+
+export type Recipe = {
     name: string;
     id: string;
     points: Point[];
 };
+
+export type RecipeUpdate = AtLeast<Recipe, 'id'>
+
+
