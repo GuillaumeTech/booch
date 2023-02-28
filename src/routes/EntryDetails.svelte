12f8649e8efc120393a14ad56ffb1d632a0a11ba
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-02-28 16:26:52 -0500

    fix details stuff

--- src/routes/EntryDetails.svelte
+++ src/routes/EntryDetails.svelte
@@ -6,53 +6,55 @@
 
 	export let pointPicked: Point, resetPickedPoint: Function;
 
-	let editablePoint = writable(pointPicked);
+	$: editablePoint = writable(pointPicked);
 	let editing = false;
 </script>
 
 <div in:fly={{ y: 10, duration: 500 }} out:fade>
-	<div>
-		<button
-			on:click={() => {
-				if (editing) {
-					points.update($editablePoint, $activeRecipe);
-				}
-				editing = !editing;
-			}}>{editing ? 'Done' : 'Edit'}</button
-		>
-		<button
-			on:click={() => {
-				if (pointPicked) {
-					points.remove(pointPicked.id, $activeRecipe); // makes ts happy but if is un-needed really
+	{#key pointPicked.id}
+		<div>
+			<button
+				on:click={() => {
+					if (editing) {
+						points.update($editablePoint, $activeRecipe);
+					}
+					editing = !editing;
+				}}>{editing ? 'Done' : 'Edit'}</button
+			>
+			<button
+				on:click={() => {
+					if (pointPicked) {
+						points.remove(pointPicked.id, $activeRecipe); // makes ts happy but if is un-needed really
+						resetPickedPoint();
+					}
+				}}>delete</button
+			>
+			<button
+				on:click={() => {
 					resetPickedPoint();
-				}
-			}}>delete</button
-		>
-		<button
-			on:click={() => {
-				resetPickedPoint();
-			}}>hide</button
-		>
-	</div>
-	<div>
-		{#if editing}
-			<form>
-				<input bind:value={$editablePoint.title} />
+				}}>hide</button
+			>
+		</div>
+		<div>
+			{#if editing}
+				<form>
+					<input bind:value={$editablePoint.title} />
+					<h4>Details</h4>
+					<input bind:value={$editablePoint.details} />
+					<h4>Funk</h4>
+					<input bind:value={$editablePoint.x} />
+					<h4>Dryness</h4>
+					<input bind:value={$editablePoint.y} />
+				</form>
+			{:else}
+				<h3>{pointPicked.title}</h3>
 				<h4>Details</h4>
-				<input bind:value={$editablePoint.details} />
+				<p>{pointPicked.details}</p>
 				<h4>Funk</h4>
-				<input bind:value={$editablePoint.x} />
+				<p>{pointPicked.x}</p>
 				<h4>Dryness</h4>
-				<input bind:value={$editablePoint.y} />
-			</form>
-		{:else}
-			<h3>{pointPicked.title}</h3>
-			<h4>Details</h4>
-			<p>{pointPicked.details}</p>
-			<h4>Funk</h4>
-			<p>{pointPicked.x}</p>
-			<h4>Dryness</h4>
-			<p>{pointPicked.y}</p>
-		{/if}
-	</div>
+				<p>{pointPicked.y}</p>
+			{/if}
+		</div>
+	{/key}
 </div>
