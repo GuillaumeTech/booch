bef24b1bd9338404124ac9acede28e1157e26bc2
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-02-28 15:45:47 -0500

    animate position change

--- src/routes/Entry.svelte
+++ src/routes/Entry.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { spring } from 'svelte/motion';
-
 	export let x = 0,
 		y = 0,
 		r = 1,
@@ -13,7 +12,12 @@
 
 	const radius = spring(r, { stiffness: 0.15, damping: 0.3 });
 	$: radius.set(r);
+	const _x = spring(x, { stiffness: 0.3, damping: 0.6 });
+	$: _x.set(x);
+
+	const _y = spring(y, { stiffness: 0.3, damping: 0.6 });
+	$: _y.set(y);
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<circle cx={x} cy={y} {id} r={$radius} {fill} {stroke} stroke-width={strokeWidth} />
+<circle cx={$_x} cy={$_y} {id} r={$radius} {fill} {stroke} stroke-width={strokeWidth} />
