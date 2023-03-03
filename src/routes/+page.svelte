0ba5a99d9db89d240ce0e1de9c62435684fb629a
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-03-03 10:43:43 -0500

    styling stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,73 +42,12 @@
 </script>
 
 <div class="app">
-	<ul>
-		{#each Object.entries($recipes) as [_, { name, id }] (id)}
+	<div class="side-bar">
+		<ul>
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			{#if editingId == id}
-				<li>
-					<input type="text" bind:value={editingName} />
-					<button
-						on:click={(e) => {
-							e.stopPropagation();
-							editRecipeName(id, editingName);
-							editingId = '';
-							editingName = '';
-						}}
-					>
-						OK
-					</button>
-				</li>
-			{:else}
+			{#if !addingNewRecipe}
 				<li
-					on:click={() => {
-						$activeRecipe = id;
-					}}
-				>
-					{name}
-					<button
-						on:click={(e) => {
-							e.stopPropagation();
-							deletingId = id;
-							deletingName = name;
-							showDeleteConfirm = true;
-						}}
-					>
-						del
-					</button>
-					<button
-						on:click={(e) => {
-							e.stopPropagation();
-							editingId = id;
-							editingName = name;
-						}}
-					>
-						edit
-					</button>
-				</li>
-			{/if}
-		{/each}
-
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		{#if !addingNewRecipe}
-			<li
-				class="add-recipe"
-				on:click={() => {
-					if (addingNewRecipe) {
-						addNewRecipe();
-						addingNewRecipe = false;
-					} else {
-						addingNewRecipe = true;
-					}
-				}}
-			>
-				{addingNewRecipe ? 'add' : ' + new recipe'}
-			</li>
-		{:else}
-			<li class="add-recipe">
-				<input type="text" bind:value={newRecipeName} />
-
-				<button
+					class="add-recipe"
 					on:click={() => {
 						if (addingNewRecipe) {
 							addNewRecipe();
@@ -116,11 +55,78 @@
 						} else {
 							addingNewRecipe = true;
 						}
-					}}>{addingNewRecipe ? 'add' : 'new recipe'}</button
+					}}
 				>
-			</li>
-		{/if}
-	</ul>
+					{addingNewRecipe ? 'add' : ' + new recipe'}
+				</li>
+			{:else}
+				<li class="add-recipe">
+					<input type="text" bind:value={newRecipeName} />
+
+					<button
+						on:click={() => {
+							if (addingNewRecipe) {
+								addNewRecipe();
+								addingNewRecipe = false;
+							} else {
+								addingNewRecipe = true;
+							}
+						}}>{addingNewRecipe ? 'add' : 'new recipe'}</button
+					>
+				</li>
+			{/if}
+			{#each Object.entries($recipes) as [_, { name, id }] (id)}
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				{#if editingId == id}
+					<li>
+						<input type="text" bind:value={editingName} />
+						<button
+							on:click={(e) => {
+								e.stopPropagation();
+								editRecipeName(id, editingName);
+								editingId = '';
+								editingName = '';
+							}}
+						>
+							OK
+						</button>
+					</li>
+				{:else}
+					<li
+						on:click={() => {
+							$activeRecipe = id;
+						}}
+					>
+						{name}
+						<button
+							on:click={(e) => {
+								e.stopPropagation();
+								deletingId = id;
+								deletingName = name;
+								showDeleteConfirm = true;
+							}}
+						>
+							del
+						</button>
+						<button
+							on:click={(e) => {
+								e.stopPropagation();
+								editingId = id;
+								editingName = name;
+							}}
+						>
+							edit
+						</button>
+					</li>
+				{/if}
+			{/each}
+		</ul>
+		<ul class="more">
+			<li>Login</li>
+			<li>About</li>
+			<li>Contact</li>
+		</ul>
+	</div>
 
 	<div class="recipe">
 		<!-- Can't render it as long as we don't have width height -->
@@ -167,31 +173,40 @@
 		height: min(65vw, 65vh);
 		margin-top: 0;
 	}
+	div.side-bar {
+		width: 25%;
+		border-right: 2px solid salmon;
+		overflow: auto;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
 	ul {
 		list-style-type: none;
 		margin: 0;
 		padding: 0;
 
-		padding-top: 0.7rem;
-		width: 25%;
-		border-right: 1px solid salmon;
-		height: 100%;
-		overflow: auto;
-
 		> li {
+			padding-top: 0.2rem;
+			padding-bottom: 0.2rem;
+
 			padding-left: 0.7rem;
 			&:hover {
 				background-color: salmon;
 				color: white;
 			}
 		}
+		&.more {
+			border-top: 2px solid salmon;
+			padding-top: 0.5rem;
+			padding-bottom: 0.5rem;
+		}
 	}
 	.app {
 		display: flex;
-		min-height: calc(100% - 3rem);
+		min-height: calc(100% - 3rem - 2px);
 	}
 	.add-recipe {
-		margin-top: 0.7rem;
-		border-top: 1px solid salmon;
+		border-bottom: 2px solid salmon;
 	}
 </style>
