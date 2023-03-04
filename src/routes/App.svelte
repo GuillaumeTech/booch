2f606134ca2c2f405c8aca59094d3b07e297cb0d
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-03-04 16:12:00 -0500

    move components

--- src/routes/App.svelte
+++ src/routes/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import type { Point } from '../types/recipe';
-	import RecipeChart from './Recipe.svelte';
+	import RecipeChart from '../components/Recipe.svelte';
 	import { recipes, activeRecipe, points } from '../store';
-	import Sidebar from './Sidebar.svelte';
+	import Sidebar from '../components/Sidebar.svelte';
+	import type { AuthSession } from '@supabase/supabase-js';
+	export let session: AuthSession;
 
 	let width: number, height: number;
 
