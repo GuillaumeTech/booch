8833fe199b38029e88b8d947db38477c863a4c2f
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-03-09 21:29:00 -0500

    feat: syncing store

--- src/stores/supabase.ts
+++ src/stores/supabase.ts
@@ -7,4 +7,25 @@ export const activeSession = writable<AuthSession | null>(null);
 
 
 
-export const syncing = writable(new Set());
+
+export const syncing = (() => {
+    const { subscribe, set, update } = writable<Set<string>>(new Set());
+
+
+    return {
+        subscribe,
+        // similar api to the Set but avoid subscribtion to reach the underlying Set Object
+        add: (toAdd: string) => {
+            update((syncing) => { return syncing.add(toAdd) })
+        },
+        remove: (toDelete: string) => {
+            update((syncing) => {
+                const newSet = syncing
+                newSet.delete(toDelete)
+                return newSet
+            })
+        },
+        reset: () => set(new Set())
+    };
+
+})()
