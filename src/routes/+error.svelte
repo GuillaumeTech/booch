b463ab35da64921850e4a254f5111e79ff08ea49
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-04-04 15:37:28 -0400

    feat: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<div>
+	<h4>Ooops ! something went wrong</h4>
+	<p>This page doesn't exist</p>
+	<a href="/"> Go back to the app</a>
+</div>
+
+<style lang="less">
+	div {
+		display: flex;
+		flex-direction: column;
+		max-width: 30rem;
+
+		gap: 1rem;
+		border: black 2px solid;
+		padding: 2rem;
+		margin: auto;
+	}
+	@media screen and (max-width: 600px) {
+		div {
+			border: transparent 2px solid;
+		}
+	}
+</style>
