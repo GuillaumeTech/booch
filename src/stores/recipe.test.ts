08965c7b200e54086c50bffaf2e916fd7d32a81c
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-04-10 18:01:26 -0400

    feat: test for duplicate + workflow that works maybe maybe

--- src/stores/recipe.test.ts
+++ src/stores/recipe.test.ts
@@ -1,20 +1,62 @@
 
-import { beforeEach, describe, expect, it } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { get } from "svelte/store";
-import { recipes } from "./recipe";
+import { recipes, points } from "./recipe";
 
+const recipeId = '123'
+const pointId = '456'
+
+vi.mock('uuid', () => {
+    return { v4: () => 'abc' }
+})
+
+const recipeFixture = {
+    name: 'test',
+    id: recipeId,
+    points: [],
+    axisNames: { x: "testX", y: 'testY' },
+    public: false,
+    created_at: new Date(),
+    description: 'test test descript',
+};
+
+const pointsFixture = {
+    x: 5,
+    y: 5,
+    id: pointId,
+    title: 'test point',
+    details: 'lalalal ',
+    chronology: [],
+    isFermenting: true
+};
 
 describe('Recipes store', () => {
     beforeEach(() => {
         recipes.set({})
     })
-
-
-    it("adds a recipe", () => {
-        recipes.add({ id: '123' })
-        expect(get(recipes)).toEqual({ id: 'test' });
+    afterEach(() => {
+        vi.clearAllMocks()
     })
 
+    it("adds a recipe", () => {
+        recipes.add(recipeFixture)
+        expect(get(recipes)).toStrictEqual({ [recipeId]: recipeFixture });
+    })
 
+    it("adds a point", () => {
+        recipes.add(recipeFixture)
+        expect(get(recipes)).toStrictEqual({ [recipeId]: recipeFixture });
+        points.add(pointsFixture, recipeId)
+        expect(get(recipes)).toStrictEqual({ [recipeId]: { ...recipeFixture, points: [pointsFixture] } });
+    })
+
+    it("duplicate a point", () => {
+        recipes.add(recipeFixture)
+        expect(get(recipes)).toStrictEqual({ [recipeId]: recipeFixture });
+        points.add(pointsFixture, recipeId)
+        expect(get(recipes)).toStrictEqual({ [recipeId]: { ...recipeFixture, points: [pointsFixture] } });
+        points.duplicate(pointId, recipeId)
+        expect(get(recipes)).toStrictEqual({ [recipeId]: { ...recipeFixture, points: [pointsFixture, { ...pointsFixture, id: 'abc' }] } });
+    })
 });
