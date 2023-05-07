c015755671640363acc57d356fa9df41d82c8599
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-05-06 22:14:45 -0400

    feat: some styling for public pages

--- src/routes/public-recipes/[page]/+page.svelte
+++ src/routes/public-recipes/[page]/+page.svelte
@@ -5,32 +5,47 @@
 	const pageNumber = Number($page.params.page);
 </script>
 
-<li>
-	{#each recipes as { id, name, description } (id)}
-		<ul>
-			<a href={`/recipe/${id}`}>{name}</a>
+<div class="recipes">
+	<li>
+		{#each recipes as { id, name, description } (id)}
+			<ul>
+				<a href={`/recipe/${id}`}>{name}</a>
 
-			{#if description}
-				<p>{description}</p>
-			{:else}
-				<p><i>no description</i></p>
-			{/if}
-		</ul>
-	{/each}
-</li>
-<div>
-	<a data-sveltekit-reload href={`/public-recipes/${pageNumber + 1}`}>Next page</a>
-	{#if pageNumber > 0}
-		<a data-sveltekit-reload href={`/public-recipes/${pageNumber - 1}`}>Previous page</a>
-	{/if}
+				{#if description}
+					<p>{description}</p>
+				{:else}
+					<p><i>no description</i></p>
+				{/if}
+			</ul>
+		{/each}
+	</li>
+	<div class="pages">
+		{#if pageNumber > 0}
+			<a data-sveltekit-reload href={`/public-recipes/${pageNumber - 1}`}>Previous page</a>
+		{/if}
+		<a class="next" data-sveltekit-reload href={`/public-recipes/${pageNumber + 1}`}>Next page</a>
+	</div>
 </div>
 
 <style lang="less">
+	div.recipes {
+		margin-left: 2rem;
+		margin-right: 2rem;
+	}
+	div.pages {
+		margin-top: 2rem;
+		display: flex;
+		flex-direction: row;
+		.next {
+			margin-left: auto;
+		}
+	}
 	li {
 		list-style-type: none;
 	}
 	ul {
 		margin-top: 2rem;
+		padding: 0;
 		a {
 			font-size: 1.2rem;
 		}
