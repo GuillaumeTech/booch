91b19b257b8f1c5d622f97b00b4bc5d0929ca290
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-05-13 13:01:46 -0400

    fix: transition bug

--- src/lib/components/EntryDetails.svelte
+++ src/lib/components/EntryDetails.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import type { AxisNames, Point } from '../../types/recipe';
 	import { points, activeRecipeId } from '../../stores/recipe';
-	import { fade, fly } from 'svelte/transition';
 	import FermentEditModal from './Modals/FermentEditModal.svelte';
 	import Timeline from './Timeline.svelte';
 	import IconButton from './Icons/IconButton.svelte';
@@ -17,99 +16,97 @@
 </script>
 
 {#if pointPicked}
-	<div in:fly={{ y: 10, duration: 500 }} out:fade>
-		{#key pointPicked.id}
-			<FermentEditModal
-				{axisNames}
-				showModal={editing}
-				point={pointPicked}
-				onCancel={() => {
-					editing = false;
-				}}
-				onOk={(point) => {
-					points.update(point, $activeRecipeId);
-					editing = false;
-				}}
-			/>
-			<AlertModal
-				showModal={showDeletingModal}
-				onCancel={() => {
-					showDeletingModal = false;
-				}}
-				onOk={() => {
-					showDeletingModal = false;
-					if (pointPicked) {
-						points.remove(pointPicked.id, $activeRecipeId);
-					}
-					resetPickedPoint();
-				}}
-			>
-				<p>
-					Are you sure about deleting <b>{pointPicked.title}</b> ? This can't be undone
-				</p></AlertModal
-			>
+	{#key pointPicked.id}
+		<FermentEditModal
+			{axisNames}
+			showModal={editing}
+			point={pointPicked}
+			onCancel={() => {
+				editing = false;
+			}}
+			onOk={(point) => {
+				points.update(point, $activeRecipeId);
+				editing = false;
+			}}
+		/>
+		<AlertModal
+			showModal={showDeletingModal}
+			onCancel={() => {
+				showDeletingModal = false;
+			}}
+			onOk={() => {
+				showDeletingModal = false;
+				if (pointPicked) {
+					points.remove(pointPicked.id, $activeRecipeId);
+				}
+				resetPickedPoint();
+			}}
+		>
+			<p>
+				Are you sure about deleting <b>{pointPicked.title}</b> ? This can't be undone
+			</p></AlertModal
+		>
 
-			<div class="title-and-options">
-				<h3>{pointPicked.title}</h3>
+		<div class="title-and-options">
+			<h3>{pointPicked.title}</h3>
 
-				{#if !readOnly}
-					<IconButton
-						iconName="edit-2"
-						fill="black"
-						stroke="black"
-						on:click={(e) => {
-							editing = !editing;
-						}}
-						data-testid={`edit-${pointPicked.title}`}
-					/>
-					<IconButton
-						iconName="trash"
-						fill="black"
-						stroke="black"
-						on:click={(e) => {
-							showDeletingModal = true;
-						}}
-						data-testid={`delete-${pointPicked.title}`}
-					/>
-				{/if}
-				<div class="hide">
-					<IconButton
-						iconName="x"
-						fill="black"
-						stroke="black"
-						strokeWidth="3"
-						on:click={() => {
-							resetPickedPoint();
-						}}
-						data-testid={`hide-${pointPicked.title}`}
-					/>
-				</div>
+			{#if !readOnly}
+				<IconButton
+					iconName="edit-2"
+					fill="black"
+					stroke="black"
+					on:click={(e) => {
+						editing = !editing;
+					}}
+					data-testid={`edit-${pointPicked.title}`}
+				/>
+				<IconButton
+					iconName="trash"
+					fill="black"
+					stroke="black"
+					on:click={(e) => {
+						showDeletingModal = true;
+					}}
+					data-testid={`delete-${pointPicked.title}`}
+				/>
+			{/if}
+			<div class="hide">
+				<IconButton
+					iconName="x"
+					fill="black"
+					stroke="black"
+					strokeWidth="3"
+					on:click={() => {
+						resetPickedPoint();
+					}}
+					data-testid={`hide-${pointPicked.title}`}
+				/>
 			</div>
-			<div>
-				<div class="point-info">
-					<div>
-						<h4>Details</h4>
-						<p data-testid={`details-${pointPicked.title}`}>{pointPicked.details}</p>
-						<div class="grades">
-							<div class="grade">
-								<h4>{axisNames.x}</h4>
-								<p>{pointPicked.x}</p>
-							</div>
-							<div class="grade">
-								<h4>{axisNames.y}</h4>
-								<p>{pointPicked.y}</p>
-							</div>
+		</div>
+		<div>
+			<div class="point-info">
+				<div>
+					<h4>Details</h4>
+					<p data-testid={`details-${pointPicked.title}`}>{pointPicked.details}</p>
+					<div class="grades">
+						<div class="grade">
+							<h4>{axisNames.x}</h4>
+							<p>{pointPicked.x}</p>
+						</div>
+						<div class="grade">
+							<h4>{axisNames.y}</h4>
+							<p>{pointPicked.y}</p>
 						</div>
 					</div>
-					<div>
-						<h4>Chronology</h4>
+				</div>
+				<div>
+					<h4>Chronology</h4>
 
-						<Timeline dates={pointPicked.chronology} />
-					</div>
+					<Timeline dates={pointPicked.chronology} />
 				</div>
 			</div>
-		{/key}
-	</div>
+		</div>
+	{/key}
 {/if}
 
 <style lang="less">
