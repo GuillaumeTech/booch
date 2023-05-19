746ccab80b7b2af4851b4b6d01dc52ca32acf3ff
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-05-18 21:34:00 -0400

    test: e2e test for no null grading fix

--- tests/points.test.ts
+++ tests/points.test.ts
@@ -11,3 +11,20 @@ test('Can create,edit and delete a point', async ({ page }) => {
 	await deletePoint(page, 'something-new');
 	await expect(page.locator('circle')).toBeHidden();
 });
+
+test('Cannot set grading to empty', async ({ page }) => {
+	await page.goto('/');
+	await createRecipe(page, 'test-recipe');
+	await createPoint(page, 'test-point', 'somedetails');
+	await editPointName(page, 'test-point', 'something-new');
+	await gradePoint(page, 'something-new');
+
+	// 	opens the edit modal
+	await page.getByTestId('something-new').click();
+	await page.getByTestId('edit-something-new').click();
+	// empties the x value	
+	await page.getByTestId('xValue-something-new').fill('');
+	// check that we can't save
+	await expect(page.getByRole('button', { name: 'OK' })).toBeDisabled();
+});
+
