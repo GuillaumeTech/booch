7388944860f315e7fac7dd29a71dc5db2abafdb3
Guillaume TÉCHER <guillaumeval.techer@gmail.com>
2023-05-25 20:37:20 -0400

    refactor: contact styling

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,16 +1,23 @@
 <div>
 	<a href="/"> Go back to the app</a>
 	<p>You have a suggestion ? Some stuff isn't working ? just want to say Hi ?</p>
-	<br />
-	<span>You can contact me at myfermentationnotes@gmail.com</span>
+	<span>You can contact me at: myfermentationnotes@gmail.com</span>
 </div>
 
 <style lang="less">
 	div {
-		padding: 0 10rem;
+		display: flex;
+		flex-direction: column;
+		max-width: 30rem;
+
+		gap: 1rem;
+		border: black 2px solid;
+		padding: 2rem;
 		margin: auto;
-		@media screen and (max-width: 800px) {
-			padding: 0 1rem;
+	}
+	@media screen and (max-width: 600px) {
+		div {
+			border: transparent 2px solid;
 		}
 	}
 
